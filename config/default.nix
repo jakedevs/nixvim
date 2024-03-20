2efe3661af15ca4fc125d2467576f8ea41d8e55f
Jake Levi <jakedevquestions@gmail.com>
2024-03-19 21:18:37 -0400

    backup

--- config/default.nix
+++ config/default.nix
@@ -3,10 +3,17 @@
   imports = [
     ./bufferline.nix
     ./cmp.nix
+    ./keymaps.nix
   ];
 
 
   colorschemes.kanagawa.enable = true;
+
+  globals.mapleader = " ";
+
+  viAlias = true;
+  vimAlias = true;
+  luaLoader.enable = true;
 
   plugins = {
     lualine.enable = true;
@@ -16,11 +23,24 @@
     nvim-autopairs.enable = true;
 
     neogit.enable = true;
+    neogit.settings.auto_show_console = false;
+    neogit.settings.integrations.diffview = true;
+    neogit.settings.integrations.telescope = true;
+    diffview.enable = true;
+    telescope.enable = true;
 
     lsp = {
       enable = true;
       servers = {
         nixd.enable = true;
+        rust-analyzer = {
+          enable = true;
+          installCargo = true;
+          installRustc = true;
+        };
+        tsserver.enable = true;
+        html.enable = true;
+        cssls.enable = true;
       };
     };
   };
---
+++ config/keymaps.nix
@@ -0,0 +1,11 @@
+{
+  keymaps = [
+    {
+      action = "<cmd>Neotree<CR>";
+      key = "<leader>e";
+      options = {
+        silent = true;
+      };
+    }
+  ];
+}
