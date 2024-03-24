32707f1fea79e255cd8637031f6bb9cf40e0b79b
Jake Levi <jakedevquestions@gmail.com>
2024-03-24 10:04:52 -0400

    backup

--- config/cmp.nix
+++ config/cmp.nix
@@ -7,11 +7,9 @@
       mapping = {
         __raw = ''
           cmp.mapping.preset.insert({
-            ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-            ['<C-f>'] = cmp.mapping.scroll_docs(4),
-            ['<C-Space>'] = cmp.mapping.complete(),
-            ['<C-e>'] = cmp.mapping.abort(),
-            ['<CR>'] = cmp.mapping.confirm({ select = true }),
+	  ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<Tab>"] = cmp.mapping.confirm({ select = true }),
           })
         '';
       };
--- config/options.nix
+++ config/options.nix
@@ -2,6 +2,11 @@
   config.options = {
     showmode = false;
     shortmess = "I";
+    smartindent = true;
+    autochdir = true;
+    cmdheight = 2;
+    shiftwidth = 2;
+    tabstop = 2;
     conceallevel = 2;
   };
 }
